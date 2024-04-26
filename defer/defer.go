49fb4d5832cdd3eafbf624774fc557651899e9dd
Humberto Lima <hlaff@cin.ufpe.br>
2024-04-26 14:44:55 -0300

    Add code to learn the defer concept

---
+++ defer/defer.go
@@ -0,0 +1,23 @@
+package defer_test
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	defer trackTime("main")()
+
+	fmt.Println("Start of main function")
+	// Simulate some work
+	time.Sleep(2 * time.Second)
+	fmt.Println("End of main function")
+}
+
+func trackTime(name string) func() {
+	start := time.Now()
+	fmt.Printf("Start of execution of %s\n", name)
+	return func() {
+		fmt.Printf("End of execution of %s. Elapsed time: %v\n", name, time.Since(start))
+	}
+}
